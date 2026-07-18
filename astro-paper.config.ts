caa0efc407c9b183aa0319861d8568489a8b6aaf
InfiniteVoidLoop <jackdang2005@gmail.com>
2026-07-18 22:41:25 +0700

    feat: add user intro at home page

--- astro-paper.config.ts
+++ astro-paper.config.ts
@@ -3,13 +3,13 @@ import { defineAstroPaperConfig } from "./src/types/config";
 export default defineAstroPaperConfig({
   site: {
     url: "https://astro-paper.pages.dev/",
-    title: "AstroPaper",
+    title: "Phuc's Blog",
     description: "A minimal, responsive and SEO-friendly Astro blog theme.",
-    author: "Sat Naing",
-    profile: "https://satna.ing",
+    author: "Dang Vo Hong Phuc",
+    profile: "https://github.com/InfiniteVoidLoop",
     ogImage: "default-og.jpg",
     lang: "en",
-    timezone: "Asia/Bangkok",
+    timezone: "Asia/Singapore",
     dir: "ltr",
   },
   posts: {
@@ -29,7 +29,7 @@ export default defineAstroPaperConfig({
     search: "pagefind",
   },
   socials: [
-    { name: "github",   url: "https://github.com/satnaing/astro-paper" },
+    { name: "github",   url: "https://github.com/InfiniteVoidLoop" },
     { name: "x",        url: "https://x.com/username" },
     { name: "linkedin", url: "https://www.linkedin.com/in/username/" },
     { name: "mail",     url: "mailto:yourmail@gmail.com" },
